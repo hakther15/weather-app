a0e61c7aabc13cf82b172a1968285ba26ac312b6
hakther15 <92819734+hakther15@users.noreply.github.com>
2021-12-07 12:14:36 -0500

    Trying to create forecast by using HTML in JS

--- src/index.js
+++ src/index.js
@@ -14,6 +14,36 @@ let tempLo = document.querySelector("#temp-lo");
 let weatherType = document.querySelector("#weather-type");
 let iconElement = document.querySelector("#icon");
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="temperature-max"> 18° </span>
+          <span class="temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function formatDate(date) {
   let hours = now.getHours();
 
@@ -125,3 +155,4 @@ fahrenheitLink.addEventListener("click", convertToFahrenheit);
 
 button.addEventListener("click", currentLocationSearch);
 getCurrentTemperatureOnCity("Cleveland");
+displayForecast();
