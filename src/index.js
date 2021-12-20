bccfe1e62e293e498da49b1f885c0fed5d2e2139
hakther15 <92819734+hakther15@users.noreply.github.com>
2021-12-19 19:30:03 -0500

    Added formatDay function to change the displayForecast function so timestamp doesn't show when the forecast is displayed.

--- src/index.js
+++ src/index.js
@@ -44,18 +44,33 @@ function formatDate(date) {
 let h1 = document.querySelector("h1");
 h1.innerHTML = formatDate(now);
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  return days[day];
+}
+
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+  let forecast = response.data.daily;
   let forecastHTML = `<div class="row">`;
-  response.data.daily.forEach(function (forecastDay, index) {
+  forecast.forEach(function (forecastDay, index) {
     if (index < 5) {
       forecastHTML =
         forecastHTML +
         `
       <div class="col-2">
-        <div class="weather-forecast-date">${formatDate(
-          new Date(forecastDay.dt * 1000)
-        )}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
